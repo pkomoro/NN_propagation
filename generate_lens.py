9eca73942dc39efb68de04b6e535f78ce4a05b34
pkomoro <komorowski.pawel@op.pl>
2025-08-06 10:44:15 +0200

    Saving changes

--- generate_lens.py
+++ generate_lens.py
@@ -11,23 +11,24 @@ if __name__ == "__main__":
 
     params.matrix_size = 2048
     params.pixel_size = 0.1
-    params.distance = 350
+    params.distance = 500
     params.focal_length = params.distance
-    frequency = 98  # GHz
+    frequency = 250  # GHz
     params.wavelength = params.get_wavelength_from_frequency(frequency)  # 98 GHz
-    radius = 187/2  # radius in mm
+    radius = 175/2  # radius in mm; 175 for PAPS; 187 mm for THz link
     n = 1.52 # refractive index of the lens material - COC
 
     phase = np.mod(get_lens_distribution(params),2*np.pi) * circle_aperture(params, radius, 1)
 
-    filename = "outs/PolFEL/Lens_v" + str(frequency) + "GHz_f" + str(params.distance) + "mm_px" + str(params.pixel_size) + "mm_r" + str(radius) + "mm.bmp"
+    filename = "outs/FENG-PAPS/Lens_v" + str(frequency) + "GHz_f" + str(params.distance) + "mm_px" + str(params.pixel_size) + "mm_r" + str(radius) + "mm.bmp"
     plt.imsave(filename, phase, cmap='gray', vmin=0, vmax=2*np.pi)
 
 
     r = np.arange(-radius, radius, params.pixel_size)
     phase_lens = np.mod(lens(r, params.focal_length, params.wavelength), 2 * np.pi)
     height_profile = phase_lens / 2 / np.pi * params.wavelength / (n - 1)
+    print("Height:", max(height_profile))
     plt.plot(r, height_profile, label='Lens height profile')
-    # plt.axis('off')
+    plt.axis('off')
     plt.savefig(filename.replace('.bmp', '.svg'))
 
