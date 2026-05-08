b123ef17ae7a9058ec7ab1daaca8b48f8c539610
pkomoro <komorowski.pawel@op.pl>
2026-05-08 07:47:15 +0200

    Saving changes

--- generate_lens.py
+++ generate_lens.py
@@ -9,26 +9,32 @@ from matplotlib import pyplot as plt
 if __name__ == "__main__":
     params = PropagationParams.get_example_propagation_data()
 
-    params.matrix_size = 2048
-    params.pixel_size = 0.1
-    params.distance = 500
+    params.matrix_size = 1024
+    params.pixel_size = 0.2
+    params.distance = 268
     params.focal_length = params.distance
-    frequency = 250  # GHz
-    params.wavelength = params.get_wavelength_from_frequency(frequency)  # 98 GHz
-    radius = 175/2  # radius in mm; 175 for PAPS; 187 mm for THz link
-    n = 1.52 # refractive index of the lens material - COC
+    frequency = 96.2  # GHz 96.2 GHz for 90 mW IMPATT, 95.3 for 900 mW IMPATT, 98 GHz for photomixing
+    params.wavelength = params.get_wavelength_from_frequency(frequency)
+    radius = 187/2  # radius in mm; 175 for PAPS; 187 mm for THz link
+    n = 1.513 # refractive index of the lens material - COC
 
-    phase = np.mod(get_lens_distribution(params),2*np.pi) * circle_aperture(params, radius, 1)
+    phase_shift = -0.02
 
-    filename = "outs/FENG-PAPS/Lens_v" + str(frequency) + "GHz_f" + str(params.distance) + "mm_px" + str(params.pixel_size) + "mm_r" + str(radius) + "mm.bmp"
+    phase = np.mod(phase_shift + get_lens_distribution(params),2*np.pi) * circle_aperture(params, radius, 1)
+
+    filename = "outs/THz-link/Lens_v" + str(frequency) + "GHz_f" + str(params.distance) + "mm_px" + str(params.pixel_size) + "mm_r" + str(radius) + "mm.bmp"
     plt.imsave(filename, phase, cmap='gray', vmin=0, vmax=2*np.pi)
 
 
-    r = np.arange(-radius, radius, params.pixel_size)
-    phase_lens = np.mod(lens(r, params.focal_length, params.wavelength), 2 * np.pi)
+    r = np.arange(-radius, radius, params.pixel_size/10)
+    phase_lens = np.mod(phase_shift + lens(r, params.focal_length, params.wavelength), 2 * np.pi)
     height_profile = phase_lens / 2 / np.pi * params.wavelength / (n - 1)
     print("Height:", max(height_profile))
-    plt.plot(r, height_profile, label='Lens height profile')
+
+    fig, ax = plt.subplots()
+
+    plt.plot(r, height_profile, c='black', linewidth=0.1)
     plt.axis('off')
+    ax.set_aspect('equal', adjustable='box')
     plt.savefig(filename.replace('.bmp', '.svg'))
 
